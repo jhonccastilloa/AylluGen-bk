ef2999b814cb1a53ab296d374a5cb76223d4e547
Jhon castillo <gpro1pro@gmail.com>
2026-09-05 21:11:18 -0500

    refactor: remove unused backend helpers

--- src/modules/index.ts
+++
@@ -1,2 +0,0 @@
-// No hay exportaciones directas de módulos todavía
-// Los módulos se importan individualmente cuando se necesitan
--- src/shared/types/json.ts
+++
@@ -1,7 +0,0 @@
-export type JsonPrimitive = string | number | boolean | null;
-
-export type JsonValue = JsonPrimitive | JsonObject | JsonValue[];
-
-export interface JsonObject {
-  [key: string]: JsonValue;
-}
